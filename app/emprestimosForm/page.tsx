24627a56af91c644766955b607a2379da5a5f91d
Gabriel Mendes <gabriel_mendessilva@hotmail.com>
2026-06-03 16:46:41 -0300

    form emprestimos, estillos e botao exclusao

--- app/emprestimosForm/page.tsx
+++ app/emprestimosForm/page.tsx
@@ -1,115 +1,213 @@
 "use client";
+import "./emprestimosForm.css";
+import { useState, useEffect } from "react";
 export default function EmprestimosForm() {
+  const [emprestimos, setEmprestimos] = useState<Emprestimo[]>([]);
+  const [material, setMaterial] = useState("");
+  const [marcaModelo, setMarcaModelo] = useState("");
+  const [bolsista, setBolsista] = useState("");
+  const [aluno, setAluno] = useState("");
+  const [matricula, setMatricula] = useState("");
+  const [dataAtual, setDataAtual] = useState("");
+  const [prazoDevolucao, setPrazoDevolucao] = useState("");
+
+  interface Emprestimo {
+    id: number;
+    material: string;
+    marcaModelo: string;
+    bolsista: string;
+    aluno: string;
+    matricula: string;
+    dataAtual: string;
+    prazoDevolucao: string;
+  }
+  function handleEmprestimo() {
+    const novoEmprestimo: Emprestimo = {
+      id: Date.now(),
+      material,
+      marcaModelo,
+      bolsista,
+      aluno,
+      matricula,
+      dataAtual,
+      prazoDevolucao,
+    };
+
+    const salvarEmprestimos = [...emprestimos, novoEmprestimo];
+
+    setEmprestimos(salvarEmprestimos);
+
+    localStorage.setItem("emprestimos", JSON.stringify(salvarEmprestimos));
+
+    setMaterial("");
+    setMarcaModelo("");
+    setBolsista("");
+    setAluno("");
+    setMatricula("");
+    setDataAtual("");
+    setPrazoDevolucao("");
+  }
+  useEffect(() => {
+    const dadosSalvos = localStorage.getItem("emprestimos");
+
+    if (!dadosSalvos) return;
+
+    const emprestimosSalvos: Emprestimo[] = JSON.parse(dadosSalvos);
+
+    setEmprestimos(emprestimosSalvos);
+  }, []);
+  function handdleExcluir(id: number) {
+    const novaLista = emprestimos.filter((e) => e.id !== id);
+    setEmprestimos(novaLista);
+    localStorage.setItem("emprestimos", JSON.stringify(novaLista));
+  }
   return (
-    <div>
-      <form className="max-w-sm mx-auto space-y-4">
-        <div>
-          <label className="block mb-2.5 text-sm font-medium text-heading">
-            {" "}
-            Material
-          </label>
-          <input
-            type="text"
-            id="visitors"
-            className="bg-neutral-secondary-medium border border-default-medium text-heading text-sm rounded-base focus:ring-brand focus:border-brand block w-full px-2.5 py-2 shadow-xs placeholder:text-body"
-            placeholder=""
-            required
-          />
-        </div>
-        <div>
-          <label className="block mb-2.5 text-sm font-medium text-heading">
-            {" "}
-            Marca/Modelo
-          </label>
-          <input
-            type="text"
-            id="visitors"
-            className="bg-neutral-secondary-medium border border-default-medium text-heading text-sm rounded-base focus:ring-brand focus:border-brand block w-full px-2.5 py-2 shadow-xs placeholder:text-body"
-            placeholder=""
-            required
-          />
-        </div>
-        <div>
-          <label className="block mb-2.5 text-sm font-medium text-heading">
-            {" "}
-            Bolsista
-          </label>
-          <input
-            type="text"
-            id="visitors"
-            className="bg-neutral-secondary-medium border border-default-medium text-heading text-sm rounded-base focus:ring-brand focus:border-brand block w-full px-2.5 py-2 shadow-xs placeholder:text-body"
-            placeholder=""
-            required
-          />
-        </div>
-        <div>
-          <label className="block mb-2.5 text-sm font-medium text-heading">
-            {" "}
-            Aluno
-          </label>
-          <input
-            type="text"
-            id="visitors"
-            className="bg-neutral-secondary-medium border border-default-medium text-heading text-sm rounded-base focus:ring-brand focus:border-brand block w-full px-2.5 py-2 shadow-xs placeholder:text-body"
-            placeholder=""
-            required
-          />
-        </div>
-        <div>
-          <label className="block mb-2.5 text-sm font-medium text-heading">
-            {" "}
-            Matricula
-          </label>
-          <input
-            type="text"
-            id="visitors"
-            className="bg-neutral-secondary-medium border border-default-medium text-heading text-sm rounded-base focus:ring-brand focus:border-brand block w-full px-2.5 py-2 shadow-xs placeholder:text-body"
-            placeholder=""
-            required
-          />
-        </div>
-        <div>
-          <label className="block mb-2.5 text-sm font-medium text-heading">
-            {" "}
-            Data atual
-          </label>
-          <input
-            type="text"
-            id="visitors"
-            className="bg-neutral-secondary-medium border border-default-medium text-heading text-sm rounded-base focus:ring-brand focus:border-brand block w-full px-2.5 py-2 shadow-xs placeholder:text-body"
-            placeholder=""
-            required
-          />
-        </div>
-        <div>
-          <label className="block mb-2.5 text-sm font-medium text-heading">
-            {" "}
-            Prazo de devolução
-          </label>
-          <input
-            type="text"
-            id="visitors"
-            className="bg-neutral-secondary-medium border border-default-medium text-heading text-sm rounded-base focus:ring-brand focus:border-brand block w-full px-2.5 py-2 shadow-xs placeholder:text-body"
-            placeholder=""
-            required
-          />
-        </div>
-      </form>
+    <div className="container-formulario">
       <div>
-        <button
-          type="submit"
-          className="w-full text-white bg-brand hover:bg-brand-hover focus:ring-4 focus:outline-none focus:ring-brand-light font-medium rounded-base text-sm px-5 py-2.5 text-center shadow-xs"
-        >
-          Salvar
-        </button>
+        <form className="max-w-sm mx-auto space-y-4">
+          <div>
+            <label className="block mb-2.5 text-sm font-medium text-heading">
+              {" "}
+              Material
+            </label>
+            <input
+              type="text"
+              value={material}
+              onChange={(e) => setMaterial(e.target.value)}
+              className="bg-neutral-secondary-medium border border-default-medium text-heading text-sm rounded-base focus:ring-brand focus:border-brand block w-full px-2.5 py-2 shadow-xs placeholder:text-body"
+              placeholder=""
+              required
+            />
+          </div>
+          <div>
+            <label className="block mb-2.5 text-sm font-medium text-heading">
+              {" "}
+              Marca/Modelo
+            </label>
+            <input
+              type="text"
+              value={marcaModelo}
+              onChange={(e) => setMarcaModelo(e.target.value)}
+              className="bg-neutral-secondary-medium border border-default-medium text-heading text-sm rounded-base focus:ring-brand focus:border-brand block w-full px-2.5 py-2 shadow-xs placeholder:text-body"
+              placeholder=""
+              required
+            />
+          </div>
+          <div>
+            <label className="block mb-2.5 text-sm font-medium text-heading">
+              {" "}
+              Bolsista
+            </label>
+            <input
+              type="text"
+              value={bolsista}
+              onChange={(e) => setBolsista(e.target.value)}
+              className="bg-neutral-secondary-medium border border-default-medium text-heading text-sm rounded-base focus:ring-brand focus:border-brand block w-full px-2.5 py-2 shadow-xs placeholder:text-body"
+              placeholder=""
+              required
+            />
+          </div>
+          <div>
+            <label className="block mb-2.5 text-sm font-medium text-heading">
+              {" "}
+              Aluno
+            </label>
+            <input
+              type="text"
+              value={aluno}
+              onChange={(e) => setAluno(e.target.value)}
+              className="bg-neutral-secondary-medium border border-default-medium text-heading text-sm rounded-base focus:ring-brand focus:border-brand block w-full px-2.5 py-2 shadow-xs placeholder:text-body"
+              placeholder=""
+              required
+            />
+          </div>
+          <div>
+            <label className="block mb-2.5 text-sm font-medium text-heading">
+              {" "}
+              Matricula
+            </label>
+            <input
+              type="text"
+              value={matricula}
+              onChange={(e) => setMatricula(e.target.value)}
+              className="bg-neutral-secondary-medium border border-default-medium text-heading text-sm rounded-base focus:ring-brand focus:border-brand block w-full px-2.5 py-2 shadow-xs placeholder:text-body"
+              placeholder=""
+              required
+            />
+          </div>
+          <div>
+            <label className="block mb-2.5 text-sm font-medium text-heading">
+              {" "}
+              Data atual
+            </label>
+            <input
+              type="text"
+              value={dataAtual}
+              onChange={(e) => setDataAtual(e.target.value)}
+              className="bg-neutral-secondary-medium border border-default-medium text-heading text-sm rounded-base focus:ring-brand focus:border-brand block w-full px-2.5 py-2 shadow-xs placeholder:text-body"
+              placeholder=""
+              required
+            />
+          </div>
+          <div>
+            <label className="block mb-2.5 text-sm font-medium text-heading">
+              {" "}
+              Prazo de devolução
+            </label>
+            <input
+              type="text"
+              value={prazoDevolucao}
+              onChange={(e) => setPrazoDevolucao(e.target.value)}
+              className="bg-neutral-secondary-medium border border-default-medium text-heading text-sm rounded-base focus:ring-brand focus:border-brand block w-full px-2.5 py-2 shadow-xs placeholder:text-body"
+              placeholder=""
+              required
+            />
+          </div>
+          <div>
+            <button
+              onClick={handleEmprestimo}
+              type="submit"
+              className="w-full rounded-md bg-blue-600 px-4 py-2 text-white transition hover:bg-blue-700"
+            >
+              Salvar
+            </button>
+          </div>
+          <div>
+            <button
+              type="submit"
+              className="w-full rounded-md bg-blue-600 px-4 py-2 text-white transition hover:bg-blue-700"
+            >
+              Salvar CSV
+            </button>
+          </div>
+        </form>
       </div>
-      <div>
-        <button
-          type="submit"
-          className="w-full text-white bg-brand hover:bg-brand-hover focus:ring-4 focus:outline-none focus:ring-brand-light font-medium rounded-base text-sm px-5 py-2.5 text-center shadow-xs"
-        >
-          Salvar CSV
-        </button>
+
+      <div className="h-150 overflow-y-auto ">
+        {emprestimos.map((emprestimo) => (
+          <div className="cards-itens" key={emprestimo.id}>
+            <h1>Material</h1>
+            <h2>{emprestimo.material}</h2>
+            <h1>Marca/Modelo</h1>
+            <h2>{emprestimo.marcaModelo}</h2>
+            <h1>Bolsista</h1>
+            <h2>{emprestimo.bolsista}</h2>
+            <h1>Aluno</h1>
+            <h2>{emprestimo.aluno}</h2>
+            <h1>Matrícula</h1>
+            <h2>{emprestimo.matricula}</h2>
+            <h1>Data Atual</h1>
+            <h2>{emprestimo.dataAtual}</h2>
+            <h1>Prazo de Devolução</h1>
+            <h2>{emprestimo.prazoDevolucao}</h2>
+            <button
+              className="w-full rounded-md bg-red-600 px-4 py-2 text-white transition hover:bg-red-700"
+              onClick={() => handdleExcluir(emprestimo.id)}
+            >
+              Excluir
+            </button>
+          </div>
+        ))}
       </div>
     </div>
   );
--- components/LoginForm/page.tsx
+++ components/LoginForm/page.tsx
@@ -1,39 +1,81 @@
+"use client";
+
+import { useState } from "react";
 import { useRouter } from "next/navigation";
+
 export default function LoginForm() {
-  const navigate = useRouter();
-  const handleNavigate = () => {
-    navigate.push("/emprestimosForm");
-  };
+  const router = useRouter();
+
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
+
+  function handleLogin(event: React.FormEvent<HTMLFormElement>) {
+    event.preventDefault();
+
+    // validação simples
+    if (email === "Midlab" && password === "123456") {
+      setError("");
+
+      // salva autenticação
+      localStorage.setItem("auth", "true");
+
+      // redireciona
+      router.push("/emprestimosForm");
+    } else {
+      setError("Credenciais inválidas");
+    }
+  }
+
   return (
-    <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <form className="max-w-sm mx-auto">
-        <div className="mb-5">
-          <label className="block mb-2.5 text-sm font-medium text-heading">
-            Usuario
+    <div className="flex min-h-screen items-center justify-center bg-zinc-100 dark:bg-black">
+      <form
+        onSubmit={handleLogin}
+        className="w-full max-w-sm rounded-lg bg-white p-6 shadow-md dark:bg-zinc-900"
+      >
+        <h1 className="mb-6 text-center text-2xl font-bold text-zinc-800 dark:text-white">
+          Login
+        </h1>
+
+        {/* Usuário */}
+        <div className="mb-4">
+          <label className="mb-2 block text-sm font-medium text-zinc-700 dark:text-zinc-200">
+            Usuário
           </label>
+
           <input
-            type="email"
-            id="email"
-            className="bg-neutral-secondary-medium border border-default-medium text-heading text-sm rounded-base focus:ring-brand focus:border-brand block w-full px-3 py-2.5 shadow-xs placeholder:text-body"
+            type="text"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            placeholder="Digite seu usuário"
+            className="w-full rounded-md border border-zinc-300 px-3 py-2 outline-none focus:border-blue-500"
             required
           />
         </div>
-        <div className="mb-5">
-          <label className="block mb-2.5 text-sm font-medium text-heading">
+
+        {/* Senha */}
+        <div className="mb-4">
+          <label className="mb-2 block text-sm font-medium text-zinc-700 dark:text-zinc-200">
             Senha
           </label>
+
           <input
             type="password"
-            id="password"
-            className="bg-neutral-secondary-medium border border-default-medium text-heading text-sm rounded-base focus:ring-brand focus:border-brand block w-full px-3 py-2.5 shadow-xs placeholder:text-body"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
             placeholder="••••••••"
+            className="w-full rounded-md border border-zinc-300 px-3 py-2 outline-none focus:border-blue-500"
             required
           />
         </div>
+
+        {/* Mensagem de erro */}
+        {error && <p className="mb-4 text-sm text-red-500">{error}</p>}
+
+        {/* Botão */}
         <button
-          onClick={handleNavigate}
           type="submit"
-          className="text-white bg-brand box-border border border-transparent hover:bg-brand-strong focus:ring-4 focus:ring-brand-medium shadow-xs font-medium leading-5 rounded-base text-sm px-4 py-2.5 focus:outline-none"
+          className="w-full rounded-md bg-blue-600 px-4 py-2 text-white transition hover:bg-blue-700"
         >
           Entrar
         </button>
